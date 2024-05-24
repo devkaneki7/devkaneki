0bc32da17bec9bee90ef6df325d0af60c046ec07
devkaneki7 <devkaneki777@gmail.com>
2024-05-24 13:53:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector("header");
-    header.style.opacity = "0";
-    setTimeout(function() {
-        header.style.opacity = "1";
-    }, 500);
-});
